811fe6b301c916960102db9df7c4a0c9da03c667
Rioba-Ian <riobafelix@gmail.com>
2024-02-20 15:25:27 +0300

    style application body

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 function App() {
  return (
-  <main class="max-w-7xl mx-auto w-4/5 bg-slate-200">
-   <h1 class="text-4xl uppercase font-bold">Alpaca Generator</h1>
-  </main>
+  <div id="app-container" class="">
+   <main class="max-w-7xl mx-auto w-4/5 mt-[8vmin]">
+    <h1 class="text-4xl uppercase font-bold">Alpaca Generator</h1>
+   </main>
+  </div>
  );
 }
 
