2ccd90f4a0e35e403f046cec4ed21596008bc4ff
Rioba-Ian <riobafelix@gmail.com>
2024-02-27 14:30:04 +0300

    control reactivity of the selected accessory and the buttons

--- src/components/AlpacaAccessories.tsx
+++ src/components/AlpacaAccessories.tsx
@@ -1,4 +1,4 @@
-import { createSignal, For, onCleanup, onMount } from "solid-js";
+import { createEffect, createSignal, For } from "solid-js";
 import alpaca from "../lib/alpaca";
 
 export default function AlpacaAccessories() {
@@ -6,40 +6,51 @@ export default function AlpacaAccessories() {
 
  const [labels] = createSignal(accessories);
  const [selectedAccessory, setSelectedAccessory] = createSignal<number>(1);
- const [styleLabels, setStyleLabels] = createSignal(
-  accessories
-   .filter((item) => item.id === selectedAccessory())
-   .forEach((item) => item.items)
- );
+ const [styleLabels, setStyleLabels] = createSignal<
+  { id: number; label: string }[]
+ >([]);
 
- function updateStyleLabels() {
+ createEffect(() => {
   setStyleLabels(
-   accessories
-    .filter((item) => item.id === selectedAccessory())
-    .forEach((item) => item.items)
+   accessories[selectedAccessory() - 1].items.map((item) => {
+    return { id: item.id, label: item.label };
+   })
   );
- }
- console.log(styleLabels());
+ });
 
  return (
   <section>
    <div id="accessories">
     <h2 class="uppercase text-xl font-bold">Accessorize the Alpaca's</h2>
-    <For each={labels()}>
-     {(label, _i) => (
-      <button
-       onClick={() => {
-        setSelectedAccessory(label.id);
-        updateStyleLabels();
-       }}
-      >
-       {label.label}
-      </button>
-     )}
-    </For>
+    <div
+     id="accesorize-controls"
+     class="w-1/2 flex items-center justify-start flex-wrap gap-4 my-4"
+    >
+     <For each={labels()}>
+      {(label, _i) => (
+       <button
+        class="btn--toggles focus:bg-blue-900 focus:text-white"
+        onClick={() => {
+         setSelectedAccessory(label.id);
+        }}
+       >
+        {label.label}
+       </button>
+      )}
+     </For>
+    </div>
    </div>
    <div id="styles">
     <h2 class="uppercase text-xl font-bold">style {selectedAccessory()}</h2>
+    <div id="accessorize-styles" class="w-1/2 space-x-3 space-y-4">
+     <For each={styleLabels()}>
+      {(item, _id) => (
+       <button class="btn--toggles focus:bg-blue-900 focus:text-white">
+        {item.label}
+       </button>
+      )}
+     </For>
+    </div>
    </div>
   </section>
  );
