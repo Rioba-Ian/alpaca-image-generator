36ece779f34baeceaac6672cc4f5044e72017f2f
Rioba-Ian <riobafelix@gmail.com>
2024-03-01 15:44:03 +0300

    reduce size for heading

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ function App() {
  return (
   <div id="app-container" class="">
    <main class="max-w-7xl mx-auto w-4/5 mt-[8vmin]">
-    <h1 class="text-4xl uppercase font-bold">Alpaca Generator</h1>
+    <h1 class="md:text-4xl uppercase font-bold text-center md:text-left ">
+     Alpaca Generator
+    </h1>
     <div id="content" class="grid grid-cols-1 md:grid-cols-2 gap-12 relative">
      <AlpacaImageGenerator />
      <AlpacaAccessories />
